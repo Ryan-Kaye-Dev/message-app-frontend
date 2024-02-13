f2587d37bce31a60326d61b8267606f1c99416e7
bisto.js <ryanrunescapes@outlook.com>
2024-02-13 22:36:09 +0000

    Added user avatar support

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,24 +1,33 @@
 const BottomBar = ({ user }) => {
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  // Construct the full URL to the user's avatar
+  const avatarUrl = import.meta.env.VITE_ENTRY_POINT + user.avatar;
+
   return (
-    <div className="bottom-bar flex justify-between items-center bg-stone-800 py-4 absolute bottom-0 w-full px-6 gap-4">
-      {user && (
-        <div className="flex justify-center items-center">
-          <img
-            src="https://randomuser.me/api/portraits"
-            alt="Avatar"
-            className="w-8 h-8 rounded-full"
-          />
-        </div>
-      )}
-      <div className="user-info flex flex-col justify-start ml-4">
-        {user && (
-          <>
-            <div>Username</div>
-            <div className="text-left">Online</div>
-          </>
-        )}
+    <div className="bottom-bar flex justify-between items-center bg-stone-800 py-4 absolute bottom-0 w-full px-6 gap-4 z-10">
+      <div className="justify-center items-center w-16 h-16">
+        <img
+          src={avatarUrl}
+          alt="Avatar"
+          className="rounded-full border-stone-400 w-16 h-16 cursor-pointer"
+        />
       </div>
-      <div className="flex justify-center items-center">Friends</div>
+      <div className="user-info flex flex-col justify-start ml-4">
+        <>
+          <div className="text-left text-lg">{user.username}</div>
+          <div className="text-left text-sm text-green-300">Online</div>{" "}
+        </>
+      </div>
+      <div className="flex justify-center items-center ml-2 p-1">
+        <img
+          className="h-6 cursor-pointer"
+          src="../../../src/assets/friends.png"
+          alt="friends"
+        />
+      </div>
       <div className="flex flex-grow justify-center items-center">
         <input
           type="text"
