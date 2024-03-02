b8aac309cb49192c67c37519d1f670085bedab8d
bisto.js <ryanrunescapes@outlook.com>
2024-03-02 16:56:40 +0000

    Modified Base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/message-app-frontend/",
+  base: "/message-app-frontend",
   plugins: [react()],
   css: {
     postcss: {
